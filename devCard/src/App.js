51112c648f10a44208f505f4686840d5615dc6a3
97hdz <70217263+97hdz@users.noreply.github.com>
2022-02-22 15:33:04 +0100

    Add files via upload

---
+++ devCard/src/App.js
@@ -0,0 +1,17 @@
+import Name from './Name'
+import Main from './Main'
+import Footer from './Footer'
+import './styles/App.css';
+
+
+function App() {
+  return (
+    <div>
+    <Name />
+    <Main />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ devCard/src/Main.js
@@ -0,0 +1,14 @@
+import './styles/Main.css';
+
+export default function Main() {
+    return (
+        <div className="main-content">
+            <div className='main-text-only'>
+            <h2 className="about">About</h2>
+            <small className="about-text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</small>
+            <h2 className="interests">Interests</h2>
+            <small className="interests-text">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</small>
+            </div>
+        </div>
+    )
+}
